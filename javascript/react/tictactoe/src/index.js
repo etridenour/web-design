98cfdba11ec23417dc5cf3e32e07ac5a7eccc9ed
etridenour <etridenour@gmail.com>
2018-09-11 14:47:37 -0500

    tuesday work

--- javascript/react/tictactoe/src/index.js
+++ javascript/react/tictactoe/src/index.js
@@ -3,29 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-class Square extends React.Component {
+function Square(props) {
     
-    render() {
       return (
-        <button className="square" onClick={() => this.props.onClick()}>
-          {this.props.value}
+        <button className="square" onClick={props.onClick}>
+          {props.value}
         </button>
       );
     }
-  }
+
   
   class Board extends React.Component {
     constructor(props){
       super(props);
       this.state = {
-        squares: Array(9).fill(null)
+        squares: Array(9).fill(null),
+        xIsNext: true
       };
     }
 
     handleClick(i) {
       const squares = this.state.squares.slice();
-      squares[i] = 'X';
-      this.setState({squares: squares});
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext
+      });
     }
 
     renderSquare(i) {
@@ -33,7 +36,7 @@ class Square extends React.Component {
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = 'Next player: X' + (this.state.xIsNext ? 'X' : 'O');
   
       return (
         <div>
